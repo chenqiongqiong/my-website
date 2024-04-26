7153be8b79bb9a382c33fa05cfe59d2918e34f78
陈琼 <chenqiongqiong@oschina.cn>
2024-04-26 15:09:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { exec } = require('node:child_process');
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(20770, { 'Content-Type': 'application/json' });
   exec('git pull');
   res.end(JSON.stringify({
     data: 'Hello World!'
