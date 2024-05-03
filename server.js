8fcc82b93d4d0af6358b982483899f4741d5a342
陈琼琼 <619591477@qq.com>
2024-05-03 17:28:15 +0800

    change server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('node:http');
 const { exec } = require('node:child_process');
 
 // Create a local server to receive data from
-const server = http.createServer((req, res) => {
-  res.writeHead(20770, { 'Content-Type': 'application/json' });
+11const server = http.createServer((req, res) => {
+  22res.writeHead(20770, { 'Content-Type': 'application/json' });
   exec('git pull');
   res.end(JSON.stringify({
     data: 'Hello World!'
