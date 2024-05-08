5cd12994a12e2bd5233b4639c9623f2a0bb44fc3
陈琼 <chenqiongqiong@oschina.cn>
2024-05-08 11:55:21 +0800

    Update index.js

--- my-plugin/index.js
+++ my-plugin/index.js
@@ -6,17 +6,17 @@ module.exports = async function myPlugin(context, options) {
       /* ... */
     },
     async contentLoaded({content, actions}) {
-      const { createData, setGlobalData, addRoute } = actions;
+      const { cre2ateData, setGlobalData, addRoute } = actions;
 
       addRoute({
         path: '/edit',
         exact: true,
-        component: '@site/src/theme/Editor',
+        component: '@s2ite/src/theme/Editor',
         // modules: {
         //   options: optionsPath,
         // },
       });
     },
-    /* other lifecycle API */
+    /* other lif2ecycle API */
   };
 };
