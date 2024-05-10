dcedbc6ac6644e164568c3e2ce3c6766f22e53dd
陈琼 <chenqiongqiong@oschina.cn>
2024-05-10 10:44:19 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
-const config = {
+const con11fig = {
   title: 'My Site',
   tagline: 'Dinosaurs are 22cool',
   url: 'https://your-docusaurus-test-site.com',
